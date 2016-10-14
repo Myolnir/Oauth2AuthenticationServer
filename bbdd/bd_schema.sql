22fb939367d89867b8b266e480d9ae6b13bb03d7
Angel Muelas <angel.muelas@gmail.com>
2016-10-14 10:06:33 +0200

    Added sql to sql schema creation

---
+++ bbdd/bd_schema.sql
@@ -0,0 +1,57 @@
+
+
+-- Volcando estructura para tabla oauth_access_token
+CREATE TABLE IF NOT EXISTS `oauth_access_token` (
+  `token_id` varchar(255) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(255) NOT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `client_id` varchar(255) DEFAULT NULL,
+  `authentication` blob,
+  `refresh_token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando estructura para tabla oauth_approvals
+CREATE TABLE IF NOT EXISTS `oauth_approvals` (
+  `userId` varchar(255) DEFAULT NULL,
+  `clientId` varchar(255) DEFAULT NULL,
+  `scope` varchar(255) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `expiresAt` timestamp NULL DEFAULT NULL,
+  `lastModifiedAt` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando estructura para tabla oauth_client_details
+CREATE TABLE IF NOT EXISTS `oauth_client_details` (
+  `client_id` varchar(255) NOT NULL,
+  `resource_ids` varchar(255) DEFAULT NULL,
+  `client_secret` varchar(255) DEFAULT NULL,
+  `scope` varchar(255) DEFAULT NULL,
+  `authorized_grant_types` varchar(255) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(255) DEFAULT NULL,
+  `authorities` varchar(255) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additional_information` varchar(4096) DEFAULT NULL,
+  `autoapprove` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando estructura para tabla oauth_code
+CREATE TABLE IF NOT EXISTS `oauth_code` (
+  `code` varchar(250) DEFAULT NULL,
+  `authentication` longblob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando estructura para tabla oauth_refresh_token
+CREATE TABLE IF NOT EXISTS `oauth_refresh_token` (
+  `token_id` varchar(255) DEFAULT NULL,
+  `token` blob,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla oauth_client_details: ~1 rows (aproximadamente)
+INSERT INTO `oauth_client_details` (`client_id`, `resource_ids`, `client_secret`, `scope`, `authorized_grant_types`, `web_server_redirect_uri`, `authorities`, `access_token_validity`, `refresh_token_validity`, `additional_information`, `autoapprove`) VALUES
+	('confidential', NULL, 'secret', 'ifttt', 'authorization_code', NULL, 'ROLE_ANONYMOUS', 0, 0, NULL, NULL),
+	('password', NULL, 'secret', 'ifttt', 'password', NULL, 'ROLE_ANONYMOUS', 0, 0, NULL, NULL);
